12b9e26d8f33147c8a8b2fa2e1b2966c03286741
Alan1805 <132144992+Alan1805@users.noreply.github.com>
2023-08-10 17:21:56 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      question: "Qual time é o maior campeão de Copas do Brasil?",
+      answers: [
+        { option: "Cruzeiro", correct: true },
+        { option: "Grêmio", correct: false },
+        { option: "São Paulo", correct: false },
+      ],
+    },
+    {
+      question: "Qual time ganhou mais vezes o Campeonato Brasileiro (Brasileirão)?",
+      answers: [
+        { option: "Flamengo", correct: false },
+        { option: "Palmeiras", correct: true },
+        { option: "Corinthians", correct: false },
+      ],
+    },
+    {
+      question: "Qual destes times nunca foi rebaixado da série A?",
+      answers: [
+        { option: "Cuiabá", correct: true },
+        { option: "Internacional", correct: false },
+        { option: "Palmeiras", correct: false },
+      ],
+    },    
+    {
+      question: "Qual o primeiro campeão da Copa do Brasil?",
+      answers: [
+        { option: "Grêmio", correct: true },
+        { option: "Flamengo", correct: false},
+        { option: "Cruzeiro", correct: false},
+      ],
+    },
+    {
+      question: "Qual jogador marcou mais gols em uma edição de Brasileirão?",
+      answers: [
+        { option: "Edmundo", correct: false },
+        { option: "Dimba", correct: false },
+        { option: "Washington", correct: true },
+      ],
+    },
+    {
+      question: "Quantos títulos de Copa do Mundo a seleção brasileira tem?",
+      answers: [
+        { option: "7", correct: false },
+        { option: "6", correct: false },
+        { option: "5", correct:  true},
+      ],
+    },
+    {
+      question: "Pós Pelé, qual maior artilheiro da seleção brasileira?",
+      answers: [
+        { option: "Ronaldo Fenômeno", correct: false },
+        { option: "Neymar", correct: true },
+        { option: "Romário", correct: false},
+      ],
+    },
+    {
+      question: "Qual o último time brasileiro campeão da Libertadores(2023)?",
+      answers: [
+        { option: "Athletico-PR", correct: false },
+        { option: "Palmeiras", correct: false },
+        { option: "Flamengo", correct: true },
+      ],
+    },
+    {
+      question: "Qual destes times nunca ganhou uma Libertadores?",
+      answers: [
+        { option: "Fluminense", correct: true },
+        { option: "Grêmio", correct: false },
+        { option: "Vasco", correct: false },
+      ],
+    },
+    {
+      question: "Qual destes times ganhou mais vezes o Mundial de Clubes?",
+      answers: [
+        { option: "Santos", correct: false },
+        { option: "São Paulo", correct: true },
+        { option: "Flamengo", correct: false },
+      ],
+    },
+  ];
